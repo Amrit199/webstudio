8ac82624cd3238fc19d6d94d5c9991a38cb5f0f1
Amrit <tamangamrit37@gmail.com>
2022-11-23 14:14:26 +0100

    protfolio title
    portfolio title's font resize

--- src/components/workList/WorkList.jsx
+++ src/components/workList/WorkList.jsx
@@ -5,7 +5,7 @@ export default function WorkList({ title, active, setSelected, id }) {
   return (
     <div>
       <li
-        className={active ? "portfolioList active" : "portfolioList"}
+        className={active ? "portfolioListe activee" : "portfolioListe"}
         onClick={() => setSelected(id)}
       >
         {title}
